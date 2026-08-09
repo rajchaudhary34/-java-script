4c3f0d621dae5b29498770915cbf7a726bffb8b3
rajchaudhary34 <rk5179314@gmail.com>
2026-08-09 12:20:03 +0000

    day2

---
+++ basics/operations.js
@@ -0,0 +1,30 @@
+// either of  other arithmetic and logicall operations  there are some special cases here in java script
+//some of them are mentioned below
+
+
+// console.log("1"+2) //12
+// console.log(1+"2")//12
+// console.log("1"+2+2)  // 122 // if string in start then all the otheer are also converted to string and treated as the same
+// console.log(1+2+"2") //32 // if its in last then the  operatoins before are performed first and after that they are treated as string
+// console.log(1+"2"+"2")//122
+// console.log(1+"2"+2) //122
+
+
+console.log(+true)//this is not an way of  writing it just for studying purposes
+console.log(+"")
+
+console.log(undefined==0)
+console.log(undefined<0)
+console.log(undefined>0)
+
+
+console.log(null==undefined)
+
+console.log(null==0)
+
+console.log(null<0)
+
+console.log(null>=0)
+
+
+///strict check not only the values but also the data types are also checked "==="
