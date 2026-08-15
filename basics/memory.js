1b801fb01ad4d461d387482831d21ca28979837c
rajchaudhary34 <rk5179314@gmail.com>
2026-08-15 09:58:23 +0000

    another push

---
+++ basics/memory.js
@@ -0,0 +1,21 @@
+let name ="raj"
+let name3= name
+name3="jalebi34"
+
+console.log(name)
+console.log(name3)
+
+
+let student=
+{
+    name:"rinkiya",
+    email:"rajpppw@gmail.com"
+}
+
+
+let student2= student
+
+
+    student2.name="rammmu"
+
+console.log(student.name)
---
+++ basics/string.js
@@ -0,0 +1,15 @@
+const name= "altmash lund "
+const num = 59
+
+// console.log(name+num + " lamboo ")
+const gang= new String("gendu")
+
+console.log(gang)
+ 
+//multiple functions can be used in the sting by using dot operator
+//trim method is used to remove unnecessary space form the string there are two more methods in it trim start and trim end
+//replace( what , with what)
+//string.includes( ) to check whether something is present or not :
+//learn about split
+
+console.log(`hello ${name}`)
