c23843d7cf6356fd2b1c45f8d5b7fe42b7f34cc0
rajchaudhary34 <rk5179314@gmail.com>
2026-08-17 14:28:43 +0000

    array done

---
+++ basics/array2.js
@@ -0,0 +1,25 @@
+// // const heroes=["ironman " , "superman"]
+// // const lheroes=["flying jatt" , "loduuu"]
+
+// // // heroes.push(lheroes)
+// // // console.log(heroes)
+// // const allheroes=heroes.concat(lheroes)
+// // console.log(allheroes)
+
+// // const bkl = [...heroes , ...lheroes ]
+// // console.log(bkl)
+
+
+// const bhangbhosda=[2,44,5,3,7,[84 ,45,6],5,7,3,[2,4,8,9,7]]
+// const solved_bhangbhosda=bhangbhosda.flat(Infinity) //here in argument you give till what depth to be made merged
+// console.log(solved_bhangbhosda)
+
+console.log(Array.isArray("raju")) // asking
+console.log(Array.from("gendu"))
+console.log(Array.from({name:"hitesh"})) // lookout
+
+let s1= 100
+let s2= 200
+let s3= 300
+
+console.log(Array.of(s1,s2,s3))//making array
