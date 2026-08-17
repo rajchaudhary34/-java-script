af041bbeadcc2d0f7c02078b9df58f9ccb9a0b66
rajchaudhary34 <rk5179314@gmail.com>
2026-08-17 14:07:56 +0000

    grinnin

---
+++ basics/array.js
@@ -0,0 +1,30 @@
+const myAr=[2,6,4,6,5,84,55]
+// const myHeros=["shaktiman",'johnny sins']
+// //java script's array are resizable
+// myAr.push(566)
+// console.log(myAr)
+// myAr.pop()
+// console.log(myAr)
+// myAr.unshift(78)
+// console.log(myAr)
+// myAr.shift()
+// console.log(myAr)
+// console.log(myAr.includes(9999))
+// console.log(myAr.indexof(6)) 
+// const newArr=myArr.join()
+// console.log(typeof newArr); 
+
+
+//slice and splice
+
+console.log("A" ,myAr)
+
+const myn1 = myAr.slice(1,3)
+console.log(myn1)
+
+console.log("B" ,myAr)
+
+const myn2 = myAr.splice(1,3)
+console.log(myn2)
+console.log(myAr)
+
